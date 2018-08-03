78cc7d6bdf30a9493987cf1f21f5d0d0ecabd7f2
azfared <42039208+azfared@users.noreply.github.com>
2018-08-03 07:44:53 +0300

    ML Editt

--- MachineLearningModel/Demo.py
+++ MachineLearningModel/Demo.py
@@ -14,10 +14,9 @@ input_video = "Hackthon.MOV"
 #Detection model zoo 
 detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2017_11_17')
 
-targeted_objects = "person"
 fps = 31.580
 width = 1920 
 height = 1080
 is_color_recognition_enabled = 0
 
-object_counting_api.cumulative_object_counting_x_axis(input_video, detection_graph, category_index, is_color_recognition_enabled, fps, width, height, 700) # 400 is the x location of the ROI Line
+object_counting_api.cumulative_object_counting_x_axis(input_video, detection_graph, category_index, is_color_recognition_enabled, fps, width, height, 700,8) # 400 is the x location of the ROI Line
